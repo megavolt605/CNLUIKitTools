68bd7eca07a3b0ff873b3bfd432eacc4fe3ca8d9
Igor Smirnov <s@ronin.pro>
2017-04-12 11:41:35 +0300

    make QR-code generator public

--- CNLUIKitTools/CNLQRCodeGenerator.swift
+++ CNLUIKitTools/CNLQRCodeGenerator.swift
@@ -9,8 +9,7 @@
 import UIKit
 
 /// Amount of excess information
-enum CNLQRCodeCorrectionLevel: String
-{
+public enum CNLQRCodeCorrectionLevel: String {
     case percent7 = "L"
     case percent15 = "M"
     case percent25 = "Q"
@@ -18,7 +17,7 @@ enum CNLQRCodeCorrectionLevel: String
 }
 
 /// Class for generation QR code
-struct CNLQRCodeGenerator {
+public struct CNLQRCodeGenerator {
     
     ///  Generates UIImage with QR code of the string with specified size and correction level
     ///
@@ -27,7 +26,7 @@ struct CNLQRCodeGenerator {
     ///   - size: Width and height of generated image
     ///   - correctionLevel: Amount of excess information
     /// - Returns: UIImage with QR code
-    static func generate(forString qrString: String, withSize size: CGFloat = 200.0, correctionLevel: CNLQRCodeCorrectionLevel = .percent25) -> UIImage? {
+    public static func generate(forString qrString: String, withSize size: CGFloat = 200.0, correctionLevel: CNLQRCodeCorrectionLevel = .percent25) -> UIImage? {
         if let stringData = qrString.data(using: String.Encoding.utf8), let qrFilter = CIFilter(name:"CIQRCodeGenerator") {
             qrFilter.setValue(stringData, forKey: "inputMessage")
             qrFilter.setValue(correctionLevel.rawValue, forKey: "inputCorrectionLevel")
